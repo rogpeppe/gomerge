b1448299fae5ab9b6a35e8df4377e0d310a15752
Roger Peppe <rogpeppe@gmail.com>
2021-03-03 14:02:43 +0000

    first stab at go.mod conflict resolution

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rogpeppe/gomerge
 
 go 1.16
 
-require github.com/rogpeppe/go-internal v1.7.1-0.20210301144926-2630b2f15b04
+require (
+	github.com/rogpeppe/go-internal v1.7.1-0.20210301144926-2630b2f15b04
+	golang.org/x/mod v0.4.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/exec"
+	"path/filepath"
+
+	"golang.org/x/mod/modfile"
 )
 
 func main() {
@@ -11,13 +15,59 @@ func main() {
 }
 
 func main1() int {
-	fmt.Printf("mergetool running\n")
+	if err := main2(); err != nil {
+		fmt.Fprintf(os.Stderr, "gomerge: %v\n", err)
+		return 1
+	}
+	return 0
+}
+
+func main2() error {
 	base, local, remote, merged := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
-	fmt.Printf("	BASE=%s\n", fileContents(base))
-	fmt.Printf("	LOCAL=%s\n", fileContents(local))
-	fmt.Printf("	REMOTE=%s\n", fileContents(remote))
-	fmt.Printf("	MERGED=%s\n", fileContents(merged))
-	return 1
+	_ = base
+	if filepath.Base(merged) != "go.mod" {
+		// TODO look at Go files too.
+		return fmt.Errorf("cannot fix conflicts in non-go.mod files (base %q)", base)
+	}
+	localMod, err := parseGoMod(local)
+	if err != nil {
+		return fmt.Errorf("cannot parse local go.mod: %v", err)
+	}
+	remoteMod, err := parseGoMod(remote)
+	if err != nil {
+		return fmt.Errorf("cannot parse remote go.mod: %v", err)
+	}
+	for _, req := range remoteMod.Require {
+		localMod.Require = append(localMod.Require, req)
+	}
+	// TODO support exclude and replace too
+	localMod.Cleanup()
+	data, err := localMod.Format()
+	if err != nil {
+		return fmt.Errorf("cannot format merged go.mod file: %v", err)
+	}
+	if err := ioutil.WriteFile(merged, data, 0666); err != nil {
+		return fmt.Errorf("cannot write merged file: %v", err)
+	}
+	c := exec.Command("go", "mod", "tidy")
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		return fmt.Errorf("go mod tidy failed: %v", err)
+	}
+	return nil
+}
+
+func parseGoMod(name string) (*modfile.File, error) {
+	data, err := ioutil.ReadFile(name)
+	if err != nil {
+		return nil, err
+	}
+	mf, err := modfile.Parse(name, data, nil)
+	if err != nil {
+		return nil, fmt.Errorf("%v (contents %q)", err, data)
+	}
+	return mf, nil
 }
 
 func fileContents(name string) string {
